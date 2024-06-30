7545a29917d10f8d0e16a23e39be411b26e515ec
27justin <me@justin.cx>
2024-06-30 10:37:21 +0200

    add: fourth initial ECH state.
    	Added a fourth ECH indicator (blue), telling the user whether
    	at least the initial page request was loaded over ECH.

--- background.js
+++ background.js
@@ -1,38 +1,52 @@
 // Object to keep track of the ECH status indexed by tabId.
 let tabECH = {};
 
+function updatePageAction(tabId) {
+	const tab = tabECH[tabId];
+	let icon, title;
+
+	if (tab.fullECH) {
+		icon = "icons/ech-active.png";
+		title = "This site and all subsequent fetches from this page used ECH";
+	} else if (tab.initialECH) {
+		icon = "icons/ech-initial.png";
+		title = "This page initially used ECH, but subsequent requests did not.";
+	} else if (tab.partialECH) {
+		icon = "icons/ech-semiactive.png";
+		title = "A subsequent request did use ECH, but not all resources and not the initial page load used ECH";
+	} else {
+		icon = "icons/ech-inactive.png";
+		title = "This site, or any subsequent fetch, did not use ECH";
+	}
+
+	browser.pageAction.setIcon({tabId, path: icon});
+	browser.pageAction.setTitle({tabId, title});
+}
+
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (changeInfo.status == 'loading') {
 		tabECH[tabId] = {
-			fullECH: true, // All resources were loaded with ECH, if true a green lock is displayed.
-			intermittentECH: false // Some resources were loaded with ECH, but not all if `fullECH` is set to false.
+			fullECH: true, // All resources were loaded with ECH
+			partialECH: false, // Some resources were loaded with ECH, but not all, including the initial page load.
+			initialECH: true, // The initial page load was done via ECH, but subsequent resources were not.
+			requests: 0
 		};
 
 		// Attach the webRequest listeners to this tab.
 		browser.webRequest.onHeadersReceived.addListener((details) => {
 			browser.webRequest.getSecurityInfo(details.requestId, {})
 				.then((tls) => {
-					if(tls.state === "insecure") browser.pageAction.hide(tabId); // Either-way, this is insecure. Might as well hide it.
-					const usedEch = tls.usedEch;
-					if(usedEch === true) {
-						tabECH[tabId].intermittentECH = true;
+					tabECH[tabId].requests++;
+
+					const usedECH = tls.usedEch;
+					tabECH[tabId].fullECH = tabECH[tabId].fullECH && usedECH;
+					tabECH[tabId].partialECH = tabECH[tabId].partialECH || usedECH;
+					if(!usedECH && tabECH[tabId].requests === 1) {
+						tabECH[tabId].initialECH = false;
 					}
 
-					tabECH[tabId].fullECH = tabECH[tabId].fullECH && usedEch;
 					browser.pageAction.show(tabId);
-
-					if(tabECH[tabId].fullECH) {
-						browser.pageAction.setIcon({tabId, path: "icons/ech-active.png"});
-						browser.pageAction.setTitle({tabId, title: "This site and all subsequent fetches from this page used ECH"});
-					}else{
-						if(tabECH[tabId].intermittentECH) {
-							browser.pageAction.setIcon({tabId, path: "icons/ech-semiactive.png"});
-							browser.pageAction.setTitle({tabId, title: "This site, or a subsequent fetch, did use ECH. But not all resources were loaded with ECH"});
-						}else{
-							browser.pageAction.setIcon({tabId, path: "icons/ech-inactive.png"});
-							browser.pageAction.setTitle({tabId, title: "This site, or any subsequent fetch, did not use ECH"});
-						}
-					}
+					updatePageAction(tabId)
 				}).catch(console.error);
 		}, {tabId, urls: ["<all_urls>"]}, ['blocking']);
 	}
